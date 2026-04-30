912c13d303eaceefce555ee522185d0f20aeeceb
Sangita Singh <sangita@dreamlogodesign.com>
2026-04-30 15:12:03 +0530

    update case status

--- src/models/report.model.js
+++ src/models/report.model.js
@@ -33,8 +33,8 @@ const reportSchema = new mongoose.Schema(
     },
     status: {
       type: Number,
-      default: 0, // 1 = approve, 0 = pending, 2 = reject
-      enum: [0, 1, 2],
+      default: 0, // 0 = pending , 3 = detected , 1 = approved / 2 = reject , 4 = document_submitted , 5 = under_review , 6 = analysis , 7 = resulation 
+      enum: [0, 1, 2, 3, 4, 5, 6, 7],
     },
     uploadedAt: {
       type: Date,
