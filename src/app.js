bbfc46b9fb727f43d5b46d2e939d48bdd41ecb5d
Sangita Singh <sangita@dreamlogodesign.com>
2026-05-12 14:39:19 +0530

    add cors policy

--- src/app.js
+++ src/app.js
@@ -25,9 +25,19 @@ app.use(helmet());
 
 // app.use(cors());
 
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "https://dreamgroupsindia.com",
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
