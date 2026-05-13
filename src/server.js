877c0094961199083aa9a7ab864a0366e77f2b1c
Sangita Singh <sangita@dreamlogodesign.com>
2026-05-13 15:02:55 +0530

    server1

--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
+// console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
+// console.log("EMAIL_USER:", process.env.EMAIL_USER);
+// console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 
 app.listen(PORT, () => {
